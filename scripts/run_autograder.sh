b7700d46dd5b25736bca999e7567833271a91c24
Jiahua Chen <jiahua@brown.edu>
2021-11-17 13:47:16 -0500

    Changed paths for scripts

--- scripts/run_autograder.sh
+++ scripts/run_autograder.sh
@@ -1,8 +1,8 @@
 # Makes TeX Log script executable
 export LC_ALL=C
-chmod +x /autograder/source/texloganalyser
+chmod +x /autograder/source/scripts/texloganalyser
 
 # Runs autograder
-cp /autograder/source/compile_and_grade.py /autograder/submission/compile_and_grade.py
+cp /autograder/source/scripts/compile_and_grade.py /autograder/submission/compile_and_grade.py
 cd /autograder/submission
 python3 compile_and_grade.py
