4d48cd08041b05d8663d7b6dafd88d397dc4de3e
Jiahua Chen <jiahua@brown.edu>
2022-03-14 13:39:03 -0400

    Added templates

--- scripts/run_autograder.sh
+++ scripts/run_autograder.sh
@@ -4,7 +4,6 @@ chmod +x /autograder/source/scripts/texloganalyser
 
 # Runs autograder
 cp /autograder/source/scripts/compile_and_grade.py /autograder/submission/compile_and_grade.py
-cp /autograder/source/templates/cs22.cls /autograder/submission/cs22.cls
-cp /autograder/source/templates/cs22ta.cls /autograder/submission/cs22ta.cls
+cp /autograder/source/templates/compile/* /autograder/submission
 cd /autograder/submission
 python3 compile_and_grade.py
