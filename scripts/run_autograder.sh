3c2d2dbf54c3c5e96ad23df435265daa945d7afb
Ilija Ivanov <ilija_ivanov@brown.edu>
2025-01-17 15:06:45 -0500

    replace files if student submits python for some reason to avoid arbitrary code execution

--- scripts/run_autograder.sh
+++ scripts/run_autograder.sh
@@ -3,8 +3,8 @@ export LC_ALL=C
 chmod +x /autograder/source/scripts/texloganalyser
 
 # Runs autograder
-cp /autograder/source/scripts/compile_and_grade.py /autograder/submission/compile_and_grade.py
-cp /autograder/source/scripts/config.py /autograder/submission/config.py
+yes | cp -rf /autograder/source/scripts/compile_and_grade.py /autograder/submission/compile_and_grade.py
+yes | cp -rf /autograder/source/scripts/config.py /autograder/submission/config.py
 cp /autograder/source/templates/compile/* /autograder/submission
 cd /autograder/submission
 python3 compile_and_grade.py
