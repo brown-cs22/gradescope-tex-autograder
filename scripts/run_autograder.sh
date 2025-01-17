69f55181a3fa80d85980a7359878453ae9f7e057
Ilija Ivanov <ilija_ivanov@brown.edu>
2025-01-17 13:39:51 -0500

    copy config file

--- scripts/run_autograder.sh
+++ scripts/run_autograder.sh
@@ -4,6 +4,7 @@ chmod +x /autograder/source/scripts/texloganalyser
 
 # Runs autograder
 cp /autograder/source/scripts/compile_and_grade.py /autograder/submission/compile_and_grade.py
+cp /autograder/source/scripts/config.py /autograder/submission/config.py
 cp /autograder/source/templates/compile/* /autograder/submission
 cd /autograder/submission
 python3 compile_and_grade.py
