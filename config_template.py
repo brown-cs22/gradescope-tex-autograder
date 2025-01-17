6a1a8152bd29454601835423af2202968ecfa778
Ilija Ivanov <ilija_ivanov@brown.edu>
2025-01-17 13:33:58 -0500

    Test auto-upload for Gradescope PDF assignment

---
+++ config_template.py
@@ -0,0 +1,7 @@
+# Fill in these details and copy the file to scripts/config.py
+
+course_id = ""
+assignment_id = ""
+username = ""
+password = ""
+question_ids = [""]
--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@
 apt update
 
 cd /autograder
-apt install -y texlive-full python3
+apt install -y texlive-full python3 python3-pip
+pip3 install bs4 requests-toolbelt pypdf
